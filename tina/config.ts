b2659b4f07008f5a622b3dd0e83eb3e67796e892
Sunwoo Kim <sunshower1127@gmail.com>
2025-12-30 15:25:11 +0900

    Update build script in package.json and upgrade pnpm lockfile version; configure Tina CMS with environment variables for client ID and token

--- tina/config.ts
+++ tina/config.ts
@@ -6,8 +6,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
 	branch,
-	clientId: null, // Get this from tina.io
-	token: null, // Get this from tina.io
+	clientId: process.env.TINA_PUBLIC_CLIENT_ID,
+	token: process.env.TINA_TOKEN,
 
 	build: {
 		outputFolder: 'admin',
